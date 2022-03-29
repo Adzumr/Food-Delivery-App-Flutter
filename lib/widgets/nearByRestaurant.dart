8b70dc643b1a47e773a835ad093345455467260c
Abdulaziz Umar Jada (Adzumr) <adzumrjada@gmail.com>
2022-03-29 16:52:14 +0100

    create restaurant screen

--- lib/widgets/nearByRestaurant.dart
+++ lib/widgets/nearByRestaurant.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app_flutter/data/data.dart';
+import 'package:food_delivery_app_flutter/screens/restaurantScreen.dart';
 import 'package:sizer/sizer.dart';
 
 class NearByRestaurants extends StatelessWidget {
@@ -10,85 +11,91 @@ class NearByRestaurants extends StatelessWidget {
     List<Widget> nearByRestaurantsList = [];
     for (var restaurant in restaurants) {
       nearByRestaurantsList.add(
-        Card(
-          margin: const EdgeInsets.symmetric(vertical: 10),
-          shape: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
-            borderSide: BorderSide(color: whiteColor),
-          ),
-          elevation: 2,
-          child: Container(
-            decoration: BoxDecoration(
+        InkWell(
+          onTap: (() => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (_) => RestaurantScreen(restaurant: restaurant)))),
+          child: Card(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            shape: OutlineInputBorder(
               borderRadius: BorderRadius.circular(20),
-              color: whiteColor,
+              borderSide: BorderSide(color: whiteColor),
             ),
-            child: Row(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image(
-                    image: AssetImage(
-                      restaurant.imageUrl!,
-                    ),
-                    height: 100.sp,
-                    width: 120.sp,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          restaurant.name!,
-                          style: TextStyle(
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          restaurant.address!,
-                          style: TextStyle(
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          overflow: TextOverflow.visible,
-                        ),
-                        const SizedBox(height: 5),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.star_sharp,
-                              color: Colors.yellow,
-                              size: 20.sp,
-                            ),
-                            Icon(
-                              Icons.star_sharp,
-                              color: Colors.yellow,
-                              size: 20.sp,
-                            ),
-                            Icon(
-                              Icons.star_sharp,
-                              color: Colors.yellow,
-                              size: 20.sp,
-                            ),
-                            Icon(
-                              Icons.star_sharp,
-                              color: Colors.yellow,
-                              size: 20.sp,
-                            ),
-                          ],
-                        )
-                      ],
+            elevation: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: whiteColor,
+              ),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image(
+                      image: AssetImage(
+                        restaurant.imageUrl!,
+                      ),
+                      height: 100.sp,
+                      width: 120.sp,
+                      fit: BoxFit.cover,
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            restaurant.name!,
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            restaurant.address!,
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            overflow: TextOverflow.visible,
+                          ),
+                          const SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star_sharp,
+                                color: Colors.yellow,
+                                size: 20.sp,
+                              ),
+                              Icon(
+                                Icons.star_sharp,
+                                color: Colors.yellow,
+                                size: 20.sp,
+                              ),
+                              Icon(
+                                Icons.star_sharp,
+                                color: Colors.yellow,
+                                size: 20.sp,
+                              ),
+                              Icon(
+                                Icons.star_sharp,
+                                color: Colors.yellow,
+                                size: 20.sp,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
