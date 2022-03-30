48298b1244242a93907672738b0a71e5142c28a4
Abdulaziz Umar Jada (Adzumr) <adzumrjada@gmail.com>
2022-03-30 11:03:38 +0100

    Update restaurantScreen.dart

--- lib/screens/restaurantScreen.dart
+++ lib/screens/restaurantScreen.dart
@@ -137,17 +137,55 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                   ),
                 ),
                 const SizedBox(height: 10.0),
-                Container(
+                SizedBox(
                   height: 269.sp,
-                  color: buttonColor,
                   child: GridView.count(
                     padding: const EdgeInsets.all(10),
                     crossAxisCount: 2,
                     children:
                         List.generate(widget.restaurant.menu!.length, (index) {
                       Food foodMenu = widget.restaurant.menu![index];
-                      return Center(
-                        child: Text(foodMenu.name!),
+                      return Card(
+                        margin: const EdgeInsets.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        elevation: 2,
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: AssetImage(foodMenu.imageUrl!),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  elevation: 2,
+                                  primary: buttonColor,
+                                  onPrimary: whiteColor,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                ),
+                                onPressed: () {},
+                                child: Text(
+                                  foodMenu.name!,
+                                  style: TextStyle(
+                                    fontSize: 16.sp,
+                                    color: whiteColor,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
                       );
                     }),
                   ),
