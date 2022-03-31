77450d30e2f718b6ad275ff914eb747c2bcb70e3
Abdulaziz Umar Jada (Adzumr) <adzumrjada@gmail.com>
2022-03-31 19:35:50 +0100

    Update cartScreen.dart

--- lib/screens/cartScreen.dart
+++ lib/screens/cartScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:food_delivery_app_flutter/data/data.dart';
+import 'package:food_delivery_app_flutter/models/order.dart';
+import 'package:sizer/sizer.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -16,7 +18,70 @@ class _CartScreenState extends State<CartScreen> {
       appBar: AppBar(
         systemOverlayStyle: SystemUiOverlayStyle.light,
         backgroundColor: buttonColor,
-        title: const Text("Cart (5)"),
+        centerTitle: true,
+        title: Text(" Cart (${currentUser.cart!.length})"),
+      ),
+      body: ListView.builder(
+        itemCount: currentUser.cart!.length,
+        itemBuilder: (BuildContext context, int index) {
+          Order order = currentUser.orders![index];
+          return Container(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: SizedBox(
+                  height: 100.sp,
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Image(
+                          height: 100.sp,
+                          width: 120.sp,
+                          fit: BoxFit.cover,
+                          image: AssetImage(order.food!.imageUrl.toString()),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            order.food!.name.toString(),
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 5.0),
+                          Text(
+                            order.restaurant!.name.toString(),
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 5.0),
+                          Text(
+                            order.quantity.toString(),
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ],
+                  )),
+            ),
+          );
+        },
       ),
     );
   }
