0cc361b8074e755b8a3c0a78bd7d86b68aa9f0f3
Abdulaziz Umar Jada (Adzumr) <adzumrjada@gmail.com>
2022-03-31 19:17:42 +0100

    Update cartScreen.dart

--- lib/screens/cartScreen.dart
+++ lib/screens/cartScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:food_delivery_app_flutter/data/data.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -10,6 +12,12 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        backgroundColor: buttonColor,
+        title: const Text("Cart (5)"),
+      ),
+    );
   }
 }
